f59393c88a0e5c417efd46321b5ac10cd5e87b1a
VincentMAndrea <74203572+VincentMAndrea@users.noreply.github.com>
2020-11-09 14:49:29 -0500

    Add files via upload

---
+++ SpendiDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SpendiDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SpendiDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SpendiDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
