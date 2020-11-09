bd159b3ec75c5ff190dc2d7844c8c3c3b0e9cdfe
VincentMAndrea <74203572+VincentMAndrea@users.noreply.github.com>
2020-11-09 14:52:11 -0500

    Add files via upload

---
+++ SpendiData/dbo/StoredProcedures/spProductGetById.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spProductGetById]
+	@Id int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT Id, ProductName, [Description], RetailPrice, QuantityInStock, IsTaxable
+	FROM dbo.Product
+	WHERE Id = @Id;
+END
+RETURN 0
